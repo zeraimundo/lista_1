196ee48c16100cbbab41b42e3bbc25239b9ffb65
JOSE RAIMUNDO BEZERRA DE CARVALHO <82219488+zeraimundo@users.noreply.github.com>
2021-10-29 18:38:22 -0300

    Questão 2

---
+++ 2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+read -p "Escreva o nome do primeiro diretório:" a
+ls $a >> out.txt
+
+read -p "Escreva o nome do primeiro diretório:" b
+ls $b >> out.txt
+
+read -p "Escreva o nome do primeiro diretório:" c
+ls $c >> out.txt
+
+read -p "Escreva o nome do primeiro diretório:" d
+ls $d >> out.txt
